2e2e94043c1e8ba9396cd4d3aa8272021028453a
Oleksandr Kozhanov <oleksandr.kozhanov@nure.ua>
2024-12-29 13:31:41 +0200

    added iot client files

---
+++ Task4/arkpz-pzpi-22-4-kozhanov-oleksandr-task4/iot-device/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// Enter your ngrok URL
+const char* baseURL = "http://b071-176-122-98-176.ngrok-free.app/api/iot";
+
+// Device credentials
+const char* deviceSecret = "test-secret";
+
+#endif
